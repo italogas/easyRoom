63aa075b33d786fc25d0980a20086b19e60f4d2e
benoni <benoni.silva@gmail.com>
2015-06-11 04:34:11 -0300

    remove trash

--- branche-2/easyroom/easyroom.wsgi
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import sys
-import logging
-logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0, "/home/ubuntu/Projects/easyroom")
-
-from easyroom import app as application
-
--- branche-2/easyroom/util.py
+++
@@ -1,4 +0,0 @@
-# coding: utf-8
-
-def decodeLatin(str_text):
-	return str(str_text.replace("\\xe9", "é").replace("\\xe1", "á").replace("\\xe3", "ã").replace("\\xed", "í").replace("\\xf3", "ó").replace("\\xf4", "ô"))
